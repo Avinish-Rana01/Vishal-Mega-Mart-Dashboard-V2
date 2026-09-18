60698776166852c82d6660ebe3d0ecab7933267b
Avinish Rana <ranaavinish72@gmail.com>
2026-09-18 15:41:45 +0530

    feat: add global application configuration constants

--- src/config/constants.js
+++ src/config/constants.js
@@ -4,7 +4,7 @@
 
 export const APP_INFO = {
   TITLE: 'VISHAL MEGA MART',
-  VERSION: '4.0.0.1',
+  VERSION: '4.0',
   COMPANY: 'Markss Infotech Ltd',
   DEVELOPER: 'TeCMi Vyapti',
   DEFAULT_YEAR: new Date().getFullYear()
